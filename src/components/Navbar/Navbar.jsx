76fb85d2731a6dfc5dcb14046287ae5315c6cfc9
Joel Trinidad <jth_2929@hotmail.com>
2022-09-06 14:06:43 -0500

    feat: navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
-import * as S from './Navbar.styles'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import * as S from "./Navbar.styles";
+import imageLogo from "../../assets/img/logo.png";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const [showMenu, setShowMenu] = useState(false)
+  console.log(showMenu);
   return (
-    <div>Navbar</div>
-  )
+    <S.NavbarContainer>
+      <S.ContainerLogo onClick={() => navigate("/")}>
+        <S.ImageLogo src={imageLogo} alt="Logo" />
+      </S.ContainerLogo>
+      <S.ContainerBarsMenu onClick={() => setShowMenu(!showMenu)}>
+        <FontAwesomeIcon icon={faBars} />
+      </S.ContainerBarsMenu>
+      <S.ContainerItems showMenu={showMenu}>
+        <p onClick={() => navigate("/about-judo")}>About</p>
+      </S.ContainerItems>
+    </S.NavbarContainer>
+  );
 }
--- src/components/Navbar/Navbar.styles.jsx
+++ src/components/Navbar/Navbar.styles.jsx
@@ -1,1 +1,54 @@
 import styled from "styled-components";
+
+export const NavbarContainer = styled.div`
+  background: rgba(20, 15, 7, 0.7);
+  padding: 0 20px;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+`;
+
+export const ContainerLogo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ImageLogo = styled.img`
+  width: 90px;
+  padding-right: 20px;
+`;
+
+export const ContainerItems = styled.div`
+  display: flex;
+  align-items: center;
+
+  p {
+    color: #fff;
+    padding: 0 13px;
+    font-weight: 500;
+    cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    display: ${(props) => (props.showMenu ? "inline-grid" : "none")};
+    position: absolute;
+    top: 80px;
+    left: 0;
+    background: rgba(20, 15, 7, 0.7);
+    padding: 10px 0 20px 0;
+    width: 100%;
+  }
+`;
+
+export const ContainerBarsMenu = styled.div`
+  display: none;
+  color: #fff;
+  font-size: 36px;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
