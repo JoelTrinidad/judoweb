d3bc2ac95b576cfdef990bbbdffc90508c7cbeda
Joel Trinidad <jth_2929@hotmail.com>
2022-09-06 18:50:42 -0500

    feat: home content added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,11 @@ import * as S from './Home.styles';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <S.HomeContainer>
+        <S.Title>¡Bienvenido!</S.Title>
+        <S.WellcomeMessage>Sea todo aquel que deseé conocer un poco sobre el Judo</S.WellcomeMessage>
+      </S.HomeContainer>
+    </>
   )
 }
