7283fba591caa5b4f7fff07f782f309be9379549
JoelTrinidad <45265732+JoelTrinidad@users.noreply.github.com>
2024-07-28 15:56:06 -0600

    home design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import jigoroStatute from './assets/img/jigoro-kano-statue.png';
+import tecnicasBackgroud from './assets/img/tecnicas-background.jpg';
+import glosarioBackgroud from './assets/img/glosario-background.jpg';
+
 function App() {
   return (
-    <div className="h-screen bg-blue-judo text-white">
+    <div className="bg-blue-judo text-white">
       <div>
         <a href="/">
           <h1 className="text-3xl font-bold underline">JudoDigital</h1>
         </a>
       </div>
-      <div>
+      <div className="h-screen">
         <div className="h-2/6 text-center py-6 font-medium">
           <p className="text-3xl pt-6 pb-8">Este proyecto es con fines de amor al judo</p>
           <p>
@@ -17,20 +20,45 @@ function App() {
           </p>
         </div>
         <div className="h-4/6">
-          <ul className="grid grid-cols-3 w-screen text-xl font-bold">
-            <li className="flex min-h-60 justify-center items-center">
-              <a href="#">
-                <div className=" relative">
-                  <img className="w-full h-full" src={jigoroStatute} alt="" />
-                  <span className="absolute z-10 left-1/2 top-1/2">Historia</span>
-                </div>
+          <ul className="grid grid-cols-3 w-screen h-full text-4xl font-bold">
+            <li className="h-full overflow-x-hidden">
+              <a
+                style={{
+                  backgroundImage: `url(${jigoroStatute})`,
+                  backgroundPosition: 'center center',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                }}
+                className="grid place-content-center h-full min-h-60 transition-all ease-in-out duration-300 grayscale hover:grayscale-0 hover:scale-105"
+                href="#">
+                Historia
               </a>
             </li>
-            <li className="flex min-h-60 justify-center items-center">
-              <a href="#">Técnicas</a>
+            <li className="h-full overflow-x-hidden">
+              <a
+                style={{
+                  backgroundImage: `url(${tecnicasBackgroud})`,
+                  backgroundPosition: 'center center',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                }}
+                className="grid place-content-center h-full min-h-60 transition-all ease-in-out duration-300 grayscale hover:grayscale-0 hover:scale-105"
+                href="#">
+                Técnicas
+              </a>
             </li>
-            <li className="flex min-h-60 justify-center items-center">
-              <a href="#">Glosario</a>
+            <li className="h-full overflow-x-hidden">
+              <a
+                style={{
+                  backgroundImage: `url(${glosarioBackgroud})`,
+                  backgroundPosition: 'center center',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                }}
+                className="grid place-content-center h-full min-h-60 transition-all ease-in-out duration-300 grayscale hover:grayscale-0 hover:scale-105"
+                href="#">
+                Glosario
+              </a>
             </li>
           </ul>
         </div>
