afb90aaed35e97bf456d9fd5cdd71e1e7bf19ecd
JoelTrinidad <45265732+JoelTrinidad@users.noreply.github.com>
2024-08-07 18:48:03 -0600

    Style: Header fixed

--- src/modules/core/components/header/index.tsx
+++ src/modules/core/components/header/index.tsx
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>
-      <Link to="/">
-        <h1 className="text-3xl font-bold underline">JudoDigital</h1>
-      </Link>
+    <div className="text-white w-full px-5 py-4 fixed">
+      <div className="flex justify-between items-center">
+        <Link to="/">
+          <h1 className="text-3xl font-bold underline">JudoDigital</h1>
+        </Link>
+      </div>
     </div>
   );
 }
