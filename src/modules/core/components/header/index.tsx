6489099ca2d195d4f87aefeab83ebefa19f4832e
JoelTrinidad <45265732+JoelTrinidad@users.noreply.github.com>
2024-08-08 21:09:22 -0600

    Feat: header nav

--- src/modules/core/components/header/index.tsx
+++ src/modules/core/components/header/index.tsx
@@ -2,12 +2,23 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div className="text-white w-full px-5 py-4 fixed">
-      <div className="flex justify-between items-center">
-        <Link to="/">
-          <h1 className="text-3xl font-bold underline">JudoDigital</h1>
-        </Link>
-      </div>
-    </div>
+    <header className="text-white w-full px-5 py-4 flex justify-between items-center fixed z-10">
+      <Link to="/">
+        <h1 className="text-3xl font-bold">JudoDigital</h1>
+      </Link>
+      <nav>
+        <ul className="text-2xl text-gray-300 flex gap-5">
+          <li className="hover:text-white transition duration-500 ease-in-out">
+            <Link to="history">Historia</Link>
+          </li>
+          <li className="hover:text-white transition duration-500 ease-in-out">
+            <Link to="techniques">Técnicas</Link>
+          </li>
+          <li className="hover:text-white transition duration-500 ease-in-out">
+            <Link to="glossary">Glosario</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
