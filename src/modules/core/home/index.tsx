5739044d6d75183f6a412522034d60863329869d
JoelTrinidad <45265732+JoelTrinidad@users.noreply.github.com>
2024-08-25 08:27:59 -0600

    Style: restructure home page

--- src/modules/core/home/index.tsx
+++ src/modules/core/home/index.tsx
@@ -6,17 +6,9 @@ import { routes } from '../constants';
 
 export default function Home() {
   return (
-    <div className="bg-gray-800 text-white">
-      <div className="h-screen">
-        <div className="h-2/6 text-center py-6 font-medium grid place-content-center">
-          <p className="text-3xl pt-6 pb-8">Este proyecto es con fines de amor al judo</p>
-          <p>
-            La información encontrada aqui es una traducción e interpretacion realizada de algunas
-            fuentes como la federacion internacional de judo o el Kodokan por lo que puede contener
-            algunos errores
-          </p>
-        </div>
-        <div className="h-4/6">
+    <div className="bg-gray-800 text-white pt-[10dvh]">
+      <div className="h-[90dvh]">
+        <div className="h-5/6">
           <ul className="grid grid-cols-3 w-full h-full text-4xl font-bold">
             <li className="h-full overflow-hidden">
               <Link
@@ -58,6 +50,14 @@ export default function Home() {
               </Link>
             </li>
           </ul>
+        </div>
+        <div className="h-1/6 text-center py-6 font-medium grid place-content-center">
+          <p className="text-3xl pb-2">Este proyecto es con fines de amor al judo</p>
+          <p className="text-xl">
+            La información encontrada aqui es una traducción e interpretacion realizada de algunas
+            fuentes como la federacion internacional de judo o el Kodokan por lo que puede contener
+            algunos errores
+          </p>
         </div>
       </div>
     </div>
